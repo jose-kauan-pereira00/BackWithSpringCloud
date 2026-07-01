5bab8c864c9113adbc906acbf8eb7f4cac5fbe34
Jose Kauan Pereira <jose.kauan.pereira00@gmail.com>
2026-07-01 20:16:51 -0300

    fix: remove unnecessary comments in astro.config.mjs

--- front-end/astro.config.mjs
+++ front-end/astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://jose-kauan-pereira00.github.io', // Troque pelo seu nome de usuário do GitHub
-  base: '/ToDoAndNotifications',
+  site: 'https://jose-kauan-pereira00.github.io', 
+  base: '/ToDoAndNotifications', // <--- Exatamente igual ao nome no seu link do GitHub
 });
-
